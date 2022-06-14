879423eedb969f7b7030b265e91682449c1d7047
Daniele Atzeni <daniele.atze@gmail.com>
2022-06-14 20:28:02 +0200

    changed FW to sniff only Probe Requests

--- firmware/main.py
+++ firmware/main.py
@@ -18,6 +18,25 @@ from networking import wifi, wifisniff, socket
 import utils.config as cfg
 from crypto import element
 
+'''
+SCAN FIELDS:
+'type',
+'subtype',
+'to_ds',
+'from_ds',
+'flags',
+'duration_id',
+'sequence_ctrl',
+'addr1',
+'addr2',
+'addr3',
+'addr4',
+'rssi',
+'channel',
+'payload_size',
+'payload'
+'''
+
 
 board.init()
 board.summary()
@@ -36,14 +55,13 @@ print("TSLog initialized!")
 
 try:
     print("Connecting to WiFi...")
-    if cfg.WIFI_NAME != "MIT" and cfg.WIFI_NAME != "MIT GUEST":
-        wifi.configure(cfg.WIFI_NAME, cfg.WIFI_PWORD)
-    else:
-        wifi.configure(cfg.WIFI_NAME, '')   # set the password to '' for MIT
+    wifi.configure(cfg.WIFI_NAME, cfg.WIFI_PASSWORD)
     wifi.start()
     print("...connected!", wifi.info())
 
-    wifisniff.configure(max_packets=1000)
+    # the only subtypes of frames are beacon and probe requests
+    # we are not interested in beacon frames (code 5), but only in probe requests (code 4)
+    wifisniff.configure(max_packets=cfg.MAX_PACKETS, mgmt_subtypes=(4,))
 
     print("Connecting to ZDM...")
     device = zdm.Agent(host="zmqtt.zdm.zerynth.com")
@@ -66,17 +84,18 @@ while True:
             print("...scanning done!")
             print("Devices found: ", len(scan_res))
 
-            cfg.PUB_DICT['ts'] = ts
             cfg.PUB_DICT['n_devices'] = len(scan_res)
             cfg.PUB_DICT['sum_rssi'] = sum([x[11] for x in scan_res])       # 11 is the index of the RSSI in the scans (look at utils/config.py)
-            # we can compute other statistics here, or we can just publish and compute them after download the data
 
-            # hash the MAC addresses and publish the results
+            # we can avoid saving a lot of fields. 
+            # we are just going to save flags, duration_id, sequence_ctr, source_mac_addr, RSSI, channel, payload_size
+            # indices relative to these fields are saved in cfg.RELEVANT_FIELDS
             for i, scan in enumerate(scan_res):
-                for j, entry in enumerate(scan):
-                    if 'addr' in cfg.SCAN_OUTPUTS[j] and entry != 'FF:FF:FF:FF:FF:FF':
-                        scan_res[i][j] = element.sha256(entry)
+                scan_res[i] = [scan[j] for j in cfg.RELEVANT_FIELDS]
+            # hashing the MAC address
+            # scan_res[3] = element.sha256(scan_res[3])
 
+            cfg.PUB_DICT['ts'] = ts
             cfg.PUB_DICT['scans'] = scan_res
 
             # publish the results
--- firmware/utils/config.py
+++ firmware/utils/config.py
@@ -9,31 +9,18 @@ DELTA_COMMIT = 1
 #====================== WiFi settings
 ### Hotspot
 #WIFI_NAME = 'Senes_cell'
-#WIFI_PWORD = 'seneshot'
+#WIFI_PASSWORD = 'seneshot'
 ### AP Mick
 #WIFI_NAME = "NETGEAR62"
 #WIFI_PASSWORD = "helpfulzoo125"
 # MIT
 WIFI_NAME = "MIT"
+WIFI_PASSWORD = ''
 
 SCAN_TIME = 5 * 1000
+MAX_PACKETS = 1000
 
-#====================== Scan outputs
-SCAN_OUTPUTS = ['type',
-            'subtype',
-            'to_ds',
-            'from_ds',
-            'flags',
-            'duration_id',
-            'sequence_ctrl',
-            'addr1',
-            'addr2',
-            'addr3',
-            'addr4',
-            'rssi',
-            'channel',
-            'payload_size',
-            'payload']
+RELEVANT_FIELDS = (4, 5, 6, 8, 11, 12, 13)
 
 #====================== Publish settings
 PUB_DICT = {}
